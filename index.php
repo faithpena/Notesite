b6f9f8eddf2f24df300b965f8b50343b6a262112
illusionMeister125 <faithpena@gmail.com>
2016-12-18 00:20:44 +0800

    Test again

--- index.php
+++ index.php
@@ -150,7 +150,7 @@
                                     <img src="http://placehold.it/320x150" alt="">
                                     <div class="caption">
                                         <h4 class="pull-right">$24.99</h4>
-                                        <h4><a href="#">First Product</a>
+                                        <h4><a href="#">FAITH THE BEST</a>
                                         </h4>
                                         <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
                                     </div>
@@ -172,7 +172,7 @@
                                     <img src="http://placehold.it/320x150" alt="">
                                     <div class="caption">
                                         <h4 class="pull-right">$64.99</h4>
-                                        <h4><a href="#">Second Product</a>
+                                        <h4><a href="#">ME DA BEST</a>
                                         </h4>
                                         <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                                     </div>
