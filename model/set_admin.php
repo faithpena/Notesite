5c8cba98704e947cd0c4837bbffb969a6b026d43
illusionMeister125 <faithpena@gmail.com>
2016-12-18 23:06:11 +0800

    renamed set_admin

--- model/set_admin.php
+++ model/set_admin.php
@@ -1,6 +1,6 @@
 <?php
 	
-	function set_user($userid, $username, $password, $email, $firstname, $lastname, $sex, $month, $day, $year)
+	function set_admin($userid, $username, $password, $email, $firstname, $lastname, $sex, $month, $day, $year)
 	{
 		# Connect to database/postgres
 		$DBH = new PDO("pgsql:host=ec2-54-235-173-161.compute-1.amazonaws.com;port=5432;dbname=d1p08lsn14og2", "wplbxvzslhxeoa", "7669306a79c6df85de352ad79d52ec650e1ebe59caf6d7fd6c090444c20fa11f");
