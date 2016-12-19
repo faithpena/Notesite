b69c85e21b8b868274849d99775f4d195816fe3f
Dominic Truelen <dominic.truelen@gmail.com>
2016-12-19 15:35:07 +0800

    Fix cart input problem

--- js/change.js
+++ js/change.js
@@ -1,48 +1,51 @@
-function change(quantity, id) {
-    $.ajax({
-        type: "POST",
-        url: "controller/update_quantity.php",
-        data: "quantity=" + quantity + "&id=" + id,
-        success: function(result) {
-            var myNode = document.getElementById("mytable");
-			while (myNode.firstChild) {
-			    myNode.removeChild(myNode.firstChild);
-			}
+function change(quantity, id, max) {
+	if (!isNaN(parseFloat(quantity)) && isFinite(quantity) && quantity <= max)
+	{
+		$.ajax({
+	        type: "POST",
+	        url: "controller/update_quantity.php",
+	        data: "quantity=" + quantity + "&id=" + id,
+	        success: function(result) {
+	            var myNode = document.getElementById("mytable");
+				while (myNode.firstChild) {
+				    myNode.removeChild(myNode.firstChild);
+				}
 
-			var node = document.createElement("th");
-			var textnode = document.createTextNode("Item");
-			node.appendChild(textnode);
-			myNode.appendChild(node);
+				var node = document.createElement("th");
+				var textnode = document.createTextNode("Item");
+				node.appendChild(textnode);
+				myNode.appendChild(node);
 
-			var node = document.createElement("th");
-			var textnode = document.createTextNode("Qty");
-			node.appendChild(textnode);
-			myNode.appendChild(node);
+				var node = document.createElement("th");
+				var textnode = document.createTextNode("Qty");
+				node.appendChild(textnode);
+				myNode.appendChild(node);
 
-			var node = document.createElement("th");
-			var textnode = document.createTextNode("Subtotal");
-			node.appendChild(textnode);
-			myNode.appendChild(node);
+				var node = document.createElement("th");
+				var textnode = document.createTextNode("Subtotal");
+				node.appendChild(textnode);
+				myNode.appendChild(node);
 
-			var node = document.createElement("th");
-			myNode.appendChild(node);
+				var node = document.createElement("th");
+				myNode.appendChild(node);
 
-			myNode.innerHTML = myNode.innerHTML + result;
+				myNode.innerHTML = myNode.innerHTML + result;
 
-			var thisNode = document.createElement("tr");
-			thisNode.appendChild(document.createElement("td"));
-			thisNode.appendChild(document.createElement("td"));
+				var thisNode = document.createElement("tr");
+				thisNode.appendChild(document.createElement("td"));
+				thisNode.appendChild(document.createElement("td"));
 
-			var a = document.createElement('a');
-			var linkText = document.createTextNode("Checkout");
-			a.appendChild(linkText);
-			a.title = "Checkout";
-			a.href = "controller/checkout.php";
-			a.className = "btn btn-default";
-			thisNode.appendChild(a);
+				var a = document.createElement('a');
+				var linkText = document.createTextNode("Checkout");
+				a.appendChild(linkText);
+				a.title = "Checkout";
+				a.href = "controller/checkout.php";
+				a.className = "btn btn-default";
+				thisNode.appendChild(a);
 
-			myNode.appendChild(thisNode);
+				myNode.appendChild(thisNode);
 
-        }
-    });
+	        }
+	    });
+	}
 };
