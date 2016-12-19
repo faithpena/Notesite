e50054f4168061b8f773713dd73b09a0781a9c44
illusionMeister125 <faithpena@gmail.com>
2016-12-19 10:27:49 +0800

    only display unique orders in order history

--- model/get_orders.php
+++ model/get_orders.php
@@ -6,7 +6,7 @@
 		$DBH = new PDO("pgsql:host=ec2-54-235-173-161.compute-1.amazonaws.com;port=5432;dbname=d1p08lsn14og2", "wplbxvzslhxeoa", "7669306a79c6df85de352ad79d52ec650e1ebe59caf6d7fd6c090444c20fa11f");
 
 		# Prepare the query
-		$STH = $DBH->prepare('SELECT * FROM order_history JOIN users using (userid) JOIN orders using (orderid) ORDER BY orderid ASC');
+		$STH = $DBH->prepare('SELECT DISTINCT * FROM order_history JOIN users using (userid) JOIN orders using (orderid) ORDER BY orderid ASC');
 		
 		# Execute the query
 		$STH->execute();
