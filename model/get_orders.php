e87981cf90e879d47bc9d1d63457b09c0bc635c2
illusionMeister125 <faithpena@gmail.com>
2016-12-19 10:22:26 +0800

    added distinct

--- model/get_orders.php
+++ model/get_orders.php
@@ -3,7 +3,7 @@
 	function get_orders()
 	{
 		# Connect to database/postgres
-$DBH = new PDO("pgsql:host=ec2-54-235-173-161.compute-1.amazonaws.com;port=5432;dbname=d1p08lsn14og2", "wplbxvzslhxeoa", "7669306a79c6df85de352ad79d52ec650e1ebe59caf6d7fd6c090444c20fa11f");
+		$DBH = new PDO("pgsql:host=ec2-54-235-173-161.compute-1.amazonaws.com;port=5432;dbname=d1p08lsn14og2", "wplbxvzslhxeoa", "7669306a79c6df85de352ad79d52ec650e1ebe59caf6d7fd6c090444c20fa11f");
 
 		# Prepare the query
 		$STH = $DBH->prepare('SELECT * FROM order_history JOIN users using (userid) JOIN orders using (orderid) ORDER BY orderid ASC');
